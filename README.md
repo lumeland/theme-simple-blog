349150ffd9bfddc02ec689e038ecc0385f5f6745
Oscar Otero <oom@oscarotero.com>
2022-06-11 18:39:17 +0200

    convert the theme to a plugin

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Simple Blog
 
 Lume theme to create a simple blog.
+
+## Install
+
+```ts
+import lume from "lume/mod.ts";
+import blog from "https://deno.land/x/lume_theme_simple_blog.ts";
+
+const site = lume();
+
+site.use(blog());
+
+export default site;
+```
--- mod.ts
+++ mod.ts
@@ -1,47 +1,66 @@
-import site from "./_config.ts";
+import date from "lume/plugins/date.ts";
+import postcss from "lume/plugins/postcss.ts";
+import terser from "lume/plugins/terser.ts";
+import codeHighlight from "lume/plugins/code_highlight.ts";
+import basePath from "lume/plugins/base_path.ts";
+import slugifyUrls from "lume/plugins/slugify_urls.ts";
+import resolveUrls from "lume/plugins/resolve_urls.ts";
 
-export interface Config {
-  location?: URL;
-}
+import type { Page, Site } from "lume/core.ts";
 
-export default function (config: Config = {}) {
-  if (config.location) {
-    site.config.location = config.location;
-  }
+export default function () {
+  return (site: Site) => {
+    // Configure the site
+    site.copy("img")
+      .use(postcss())
+      .use(terser())
+      .use(date())
+      .use(codeHighlight())
+      .use(basePath())
+      .use(slugifyUrls({ alphanumeric: false }))
+      .use(resolveUrls())
+      .preprocess([".md"], (page: Page) => {
+        page.data.excerpt ??= page.data.content.split("<!--more-->")[0];
+      });
 
-  // Add remote files
-  const files = [
-    "_includes/css/navbar.css",
-    "_includes/css/page.css",
-    "_includes/css/post-list.css",
-    "_includes/css/post.css",
-    "_includes/css/reset.css",
-    "_includes/css/variables.css",
-    "_includes/layouts/archive.njk",
-    "_includes/layouts/base.njk",
-    "_includes/layouts/post.njk",
-    "_includes/layouts/tag.njk",
-    "_includes/templates/post-details.njk",
-    "_includes/templates/post-list.njk",
-    "posts/_data.yml",
-    "_data.yml",
-    "404.md",
-    "about.md",
-    "archive.tmpl.js",
-    "feed.tmpl.js",
-    "feed.xml.njk",
-    "index.njk",
-    "robots.txt.njk",
-    "sitemap.xml.njk",
-    "styles.css",
-    "tag.tmpl.js",
-  ];
+    // Add remote files
+    const files = [
+      "_includes/css/navbar.css",
+      "_includes/css/page.css",
+      "_includes/css/post-list.css",
+      "_includes/css/post.css",
+      "_includes/css/reset.css",
+      "_includes/css/variables.css",
+      "_includes/layouts/archive.njk",
+      "_includes/layouts/base.njk",
+      "_includes/layouts/post.njk",
+      "_includes/layouts/tag.njk",
+      "_includes/templates/post-details.njk",
+      "_includes/templates/post-list.njk",
+      "posts/_data.yml",
+      "_data.yml",
+      "404.md",
+      "about.md",
+      "archive.tmpl.js",
+      "feed.tmpl.js",
+      "feed.xml.njk",
+      "index.njk",
+      "robots.txt.njk",
+      "sitemap.xml.njk",
+      "styles.css",
+      "tag.tmpl.js",
+    ];
 
-  const base = new URL("./", import.meta.url);
+    const base = new URL("./", import.meta.url);
 
-  for (const file of files) {
-    site.remoteFile(file, new URL(file, base).href);
-  }
+    for (const file of files) {
+      site.remoteFile(file, new URL(file, base).href);
+    }
 
-  return site;
+    // Highlight.js stylesheet
+    site.remoteFile(
+      "_includes/css/code.css",
+      "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.0.0/build/styles/github-dark.min.css",
+    );
+  };
 }
