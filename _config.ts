d9674d803bdcd201930fae3b835d55ad9d164ce5
jrson83 <me@jrson.de>
2022-11-21 23:25:51 +0100

    added toc plugin & heading anchor css

--- _config.ts
+++ _config.ts
@@ -1,8 +1,16 @@
 import lume from "lume/mod.ts";
 import plugins from "./plugins.ts";
+import toc from "https://deno.land/x/lume_markdown_plugins@v0.1.0/toc/mod.ts";
+
+const markdown = {
+  plugins: [toc],
+  keepDefaultPlugins: true,
+};
 
 const site = lume({
   src: "./src",
+}, {
+  markdown,
 });
 
 site.use(plugins());
